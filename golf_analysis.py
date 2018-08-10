fix sequential and start covariance

golf_analysis.py
@@ -7,10 +7,13 @@ import re
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
 import scipy.stats as stats
 import numpy as np
+from sklearn.linear_model import LogisticRegression
 import os
+import math
+
 import argparse as ap
 
 BASE_DATA_PATH = 'data/current_data_files/golf/'
@@ -63,6 +66,34 @@ def plot_heatmap(df):
     plt.savefig(fig_save_path, format=SAVE_FORMAT, dpi=500)
 
 
+def get_made_cut_X_y_df(df):
+    made_round_4_df = df[df['Round'] == 4]
+    made_cut_player_list = made_round_4_df['Player'].unique().tolist()
+
+    drives = df[['Player', 'Total']][(df['Round'] == 1) & (df['Type'] == 'drive_dist')]
+    drives2 = df[['Player', 'Total']][(df['Round'] == 2) & (df['Type'] == 'drive_dist')]
+    prelim_drives = pd.concat([drives, drives2])
+
+    putts = df[['Player', 'Total']][(df['Round'] == 1) & (df['Type'] == 'putts')]
+    putts2 = df[['Player', 'Total']][(df['Round'] == 2) & (df['Type'] == 'putts')]
+    prelim_putts = pd.concat([putts, putts2])
+
+    reg_df = pd.merge(prelim_putts, prelim_drives, on='Player', how='outer')
+
+    columns = ['Player', 'Putts', 'Drive_Distance']
+    reg_df.columns = columns
+
+    scaler = MinMaxScaler()
+    reg_df[['Putts', 'Drive_Distance']] = scaler.fit_transform(reg_df[['Putts', 'Drive_Distance']])
+    reg_df['Made_Cut'] = np.where(reg_df['Player'].isin(made_cut_player_list), 1, 0)
+
+    pd.to_pickle(reg_df, '../forClustering')
+    y = np.where(reg_df['Player'].isin(made_cut_player_list), 1, 0)
+    X = reg_df.drop(['Player'], axis=1)
+
+    return X, y
+
+
 def get_round_df(df, type):
     round_1_totals = df['Total'][(df['Round'] == 1) & (df['Type'] == type)]
     round_2_totals = df['Total'][(df['Round'] == 2) & (df['Type'] == type)]
@@ -105,10 +136,12 @@ def plot_pairplot(df):
     print("DEBUG")
 
 
-def is_numeric(element):
-
-
-    pass
+def is_numeric(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
 
 
 def expand_latex_c(num_columns):
@@ -148,16 +181,15 @@ def print_latex_table(df_start, yr='UNK', caption='BLANK', label='BLANK', index_
     for index, row in df.iterrows():
         print("\\hline")
         for i in range(num_cols - 1):
-            if re.match("^\d+?\.\d+?$", str(row[col_list[i]])) is None:
+            if not is_numeric(str(row[col_list[i]])):
                 print(f"{row[col_list[i]]} & ", end="")
             else:
                 print(f"{row[col_list[i]]:.3f} & ", end='')
-        if re.match("^\d+?\.\d+?$", str(row[col_list[-1]])) is None:
+        if not is_numeric(str(row[col_list[-1]])):
             print(f"{row[col_list[-1]]}")
         else:
             print(f"{row[col_list[-1]]:.3f}\\\\")
     print("\\hline\n\\end{tabular}\n\\end{table*}\n")
-    DEBUG = 12
 
 
 def get_descriptive_stats(main_df, prev_df):
@@ -203,7 +235,30 @@ def get_descriptive_stats(main_df, prev_df):
             print_latex_table(summary_df, caption=cap, label=curr_label, yr=curr_year, index_name='Round')
 
 
-def test_significance(main_df, prev_df):
+def test_year_significance(main_df, prev_df):
+    df_list = [main_df, prev_df]
+
+    round_scores_main_df, _ = get_round_df(main_df, 'scores')
+    round_scores_prev_df, _ = get_round_df(prev_df, 'scores')
+
+    least_num_rows = min(round_scores_main_df.shape[0], round_scores_prev_df.shape[0])
+
+    # 2017 had an extra 2 players
+    round_scores_prev_df.drop(round_scores_prev_df.tail(2).index, inplace=True)
+
+    main_list = []
+    for col in round_scores_main_df:
+        main_list.extend(round_scores_main_df[col])
+
+    prev_list = []
+    for col in round_scores_prev_df:
+        prev_list.extend(round_scores_prev_df[col])
+
+    t_test = stats.ttest_ind(main_list, prev_list)
+    print("DEBUG")
+
+
+def test_round_significance(main_df, prev_df):
     # Round 1 vs each other round
 
     df_list = [main_df, prev_df]
@@ -280,6 +335,142 @@ def test_significance(main_df, prev_df):
     DEBUG = 256
 
 
+def plot_made_cut_regression(df):
+    df.dropna(axis=0, inplace=True)
+    df = df.loc[:, ~df.columns.str.contains('^Unnamed')]
+
+    X, y = get_made_cut_X_y_df(df)
+
+    DEBUG = 12
+
+
+def hole_to_hole(frame):
+    score_dict = {6: 'bogey', 5: 'bogey', 4: 'bogey', 3: 'bogey', 2: 'bogey', 1: 'bogey', 0: 'par', -1: 'birdie',
+                  -2: 'birdie', -3: 'birdie'}
+
+    scores_by_hole_df = frame.drop(['In', 'Out'], axis=1)
+    pars = scores_by_hole_df.iloc[0, 2:20]
+    scores_by_hole_df = scores_by_hole_df[scores_by_hole_df['Type'] == 'scores']
+    relative_score_df = scores_by_hole_df.iloc[1:, 2:20].apply(lambda row: row - pars, axis=1)
+    relative_score_df = relative_score_df.reset_index(drop=True)
+
+    relative_score_df = relative_score_df.replace(score_dict)
+    relative_score_df['padding'] = 'null'
+
+    df = relative_score_df.stack().T
+    df = df.reset_index(0).reset_index(drop=True)
+
+    df = df.drop(['level_0'], axis=1)
+    df.columns = ['first_hole']
+    df['second_hole'] = df.shift(-1)
+
+    df = df[df.first_hole != 'null']
+    df = df[df.second_hole != 'null']
+
+    score_table = pd.crosstab(df.first_hole, df.second_hole, margins=True)
+    index_list = ['birdie', 'par', 'bogey', 'All']
+    col_list = ['birdie', 'par', 'bogey', 'All']
+    score_table = score_table[col_list]
+    score_table = score_table.loc[index_list]
+    score_table.index = ['birdie', 'par', 'bogey', 'col_totals']
+    score_table.columns = ['birdie', 'par', 'bogey', 'row_totals']
+
+    observed = score_table.iloc[0:3, 0:3]
+
+    n = score_table.iloc[3, 3]
+    expected = np.outer(score_table["row_totals"][0:3],
+                        score_table.loc["col_totals"][0:3]) / n
+
+    expected = pd.DataFrame(expected)
+    expected.columns = ['birdie', 'par', 'bogey']
+    expected.index = ['birdie', 'par', 'bogey']
+
+    chi_squared_stat = (((observed - expected) ** 2) / expected).sum().sum()
+    print(f"Chi Squared Test Statistic = {chi_squared_stat}")
+
+    crit = stats.chi2.ppf(q=0.95,  # Find the critical value for 95% confidence*
+                          df=4)  # *
+
+    print(f"Critical value = {crit}")
+
+    p_value = 1 - stats.chi2.cdf(x=chi_squared_stat,  # Find the p-value
+                                 df=4)
+    print(f"P value = {p_value}")
+
+    DEBUG = 28
+
+
+def gather_sequential_data(df):
+    scores_by_hole_df = df.drop(['In', 'Out'], axis=1)
+    pars = scores_by_hole_df.iloc[0, 2:20]
+    scores_by_hole_df = scores_by_hole_df[scores_by_hole_df['Type'] == 'scores']
+    relative_score_df = scores_by_hole_df.iloc[1:, 2:20].apply(lambda row: row - pars, axis=1)
+    relative_score_df = relative_score_df.reset_index(drop=True)
+    observed = np.zeros((3, 3))
+    score_values = relative_score_df.values
+
+    for x in range(score_values.shape[0] - 1):
+        for y in range(score_values.shape[1] - 2):
+            i = x + 1
+            j = y + 1
+            if score_values[x, y] < 0 and score_values[i, j] < 0:
+                observed[0, 0] = observed[0, 0] + 1
+            elif score_values[x, y] < 0 and score_values[i, j] == 0:
+                observed[0, 1] = observed[0, 1] + 1
+            elif score_values[x, y] < 0 and score_values[i, j] > 0:
+                observed[0, 2] = observed[0, 2] + 1
+
+            elif score_values[x, y] == 0 and score_values[i, j] < 0:
+                observed[1, 0] = observed[1, 0] + 1
+            elif score_values[x, y] == 0 and score_values[i, j] == 0:
+                observed[1, 1] = observed[1, 1] + 1
+            elif score_values[x, y] == 0 and score_values[i, j] > 0:
+                observed[1, 2] = observed[1, 2] + 1
+
+            elif score_values[x, y] > 0 and score_values[i, j] < 0:
+                observed[2, 0] = observed[2, 0] + 1
+            elif score_values[x, y] > 0 and score_values[i, j] == 0:
+                observed[2, 1] = observed[2, 1] + 1
+            elif score_values[x, y] > 0 and score_values[i, j] > 0:
+                observed[2, 2] = observed[2, 2] + 1
+
+    contingency_table = pd.DataFrame(data=observed, index=['Bog_plus', 'Par', 'Bird-'],
+                                     columns=['Bog_plus', 'Par', 'Bird-'])
+    contingency_table['Row_Totals'] = contingency_table.sum(axis=1)
+    contingency_table.loc['Col_Totals'] = contingency_table.sum(axis=0)
+
+    total = contingency_table.iloc[3, 3]
+    expected = np.outer(contingency_table['Row_Totals'][0:3], contingency_table.loc['Col_Totals'][0:3]) / total
+    expected = pd.DataFrame(data=expected, index=['Bog_plus', 'Par', 'Bird-'],
+                            columns=['Bog_plus', 'Par', 'Bird-'])
+
+    chi_squared_stat = (((observed - expected) ** 2) / expected).sum().sum()
+    print(f"Chi Squared Test Statistic = {chi_squared_stat}")
+
+    crit = stats.chi2.ppf(q=0.95,  # Find the critical value for 95% confidence*
+                          df=4)  # *
+
+    print(f"Critical value = {crit}")
+
+    p_value = 1 - stats.chi2.cdf(x=chi_squared_stat,  # Find the p-value
+                                 df=4)
+    print(f"P value = {p_value}")
+
+    DEBUG = 2
+
+
+def hole_independence(df):
+    scores_by_hole_df = df.drop(['In', 'Out'], axis=1)
+    pars = scores_by_hole_df.iloc[0, 2:20]
+    scores_by_hole_df = scores_by_hole_df[scores_by_hole_df['Type'] == 'scores']
+    scores_by_hole_df = scores_by_hole_df.drop('Round', axis=1)
+    hole_average_df = scores_by_hole_df.groupby('Player').mean()
+
+    covariance_matrix = np.cov(hole_average_df.values)
+
+    DEBUG = 25
+
+
 def main():
     ###################################################
     ####          Load and clean csv files         ####
@@ -309,12 +500,17 @@ def main():
     totals_df = pd.concat([total_scores_series, total_putts_series, total_drives_series], axis=1)
     totals_df.columns = ['scores', 'putts', 'drive_dist']
 
-    # Logit / Probit
     # main_year_stats = get_descriptive_stats(df, prev_year_df)
     # plot_pairplot(totals_df)
     # plot_violin(rounds_scores_df)
     # plot_heatmap(rounds_scores_df)
-    test_significance(df, prev_year_df)
+    # test_round_significance(df, prev_year_df)
+    # test_year_significance(df, prev_year_df)
+
+    # gather_sequential_data(df)
+    hole_to_hole(df)
+    hole_independence(df)
+    plot_made_cut_regression(full_df)
     DEBUG = 12
 
 